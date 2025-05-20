8199b0a916e5011bbe9bf3802e11e09d5a28f201
RolfErFloor <144017838+RolfErFloor@users.noreply.github.com>
2025-05-20 14:30:33 +0200

    moved logic for asteroid processor

--- Asteroids/src/main/java/dk/sdu/mmmi/cbse/IEntityProcessor/EntityProcessor.java
+++ Asteroids/src/main/java/dk/sdu/mmmi/cbse/IEntityProcessor/EntityProcessor.java
@@ -1,5 +1,7 @@
 package dk.sdu.mmmi.cbse.IEntityProcessor;
 
+import dk.sdu.mmmi.cbse.common.asteroids.Asteroid;
+import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
@@ -8,5 +10,29 @@ public class EntityProcessor implements IEntityProcessingService {
     @Override
     public void process(GameData gameData, World world) {
         System.out.println("ASTREROID ENTITY PROCESS");
+        for (Entity asteroid : world.getEntities(Asteroid.class)) {
+            double changeX = Math.cos(Math.toRadians(asteroid.getRotation()));
+            double changeY = Math.sin(Math.toRadians(asteroid.getRotation()));
+
+            asteroid.setX(asteroid.getX() + changeX * 0.5);
+            asteroid.setY(asteroid.getY() + changeY * 0.5);
+
+            if (asteroid.getX() < 0) {
+                asteroid.setX(gameData.getDisplayWidth());
+            }
+
+            if (asteroid.getX() > gameData.getDisplayWidth()) {
+                asteroid.setX(0);
+            }
+
+            if (asteroid.getY() < 0) {
+                asteroid.setY(gameData.getDisplayHeight());
+            }
+
+            if (asteroid.getY() > gameData.getDisplayHeight()) {
+                asteroid.setY(0);
+            }
+
+        }
     }
 }
